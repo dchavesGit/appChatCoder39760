bbab52229e4620bbea2a9f2f65e7a4770cc97ef2
dchavesGit <chaves.darionehuen@gmail.com>
2023-05-17 22:16:06 -0300

    segundo checkpoint

--- src/app.js
+++ src/app.js
@@ -10,5 +10,22 @@ app.engine("handlebars", handlebars.engine());
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "handlebars");
 app.use("/", viewsRouter);
+
 const server = app.listen(8080, () => console.log("Server running"));
 const io = new Server(server);
+
+const messages = [];
+
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
+
+  socket.on("message", (data) => {
+    messages.push(data);
+    io.emit("messageLogs", messages);
+  });
+
+  socket.on("authenticated", (data) => {
+    socket.emit("messageLogs", messages);
+    socket.broadcast.emit("newUserConnected", data);
+  });
+});
